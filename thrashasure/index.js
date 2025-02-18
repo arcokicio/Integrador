b3d344d037781575d5034ba3c5237ed826dd1a19
arcokicio <151792143+arcokicio@users.noreply.github.com>
2025-02-17 21:10:48 -0300

    Add files via upload

--- thrashasure/index.js
+++ thrashasure/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Middleware
 app.use(bodyParser.json());
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',           
-  password: 'password',   
+  password: 'admin',   
   database: 'frutaria',   
 });
 
